d9dce7647646dd1cc89e2b3231f0c1852a377824
spike <spike@hell-box.homenet.telecomitalia.it>
2014-07-18 14:39:06 +0200

    fixed testing

--- src/trollan/lexer_test.go
+++ src/trollan/lexer_test.go
@@ -5,22 +5,27 @@ import "bufio"
 import "strings"
 
 func TestSimple(t *testing.T) {
-	buf := strings.NewReader("test_test test123 1234 1234.12")
+	buf := strings.NewReader("test_test test123 1234 1234.12 \"test string\"")
 	l := NewLexer (bufio.NewReader (buf))
 
 	tok, err := l.NextToken()
 	if err != nil { t.Errorf(err.Error()) }
-	if tok.Type != TOK_ID && tok.Val != "test_test" { t.Errorf("%s", tok) }
+	if tok.Type != TOK_ID || tok.Val != "test_test" { t.Errorf("%s", tok) }
 	
 	tok, err = l.NextToken()
 	if err != nil { t.Errorf(err.Error()) }
-	if tok.Type != TOK_ID && tok.Val != "test123" { t.Errorf("%s", tok) }
+	if tok.Type != TOK_ID || tok.Val != "test123" { t.Errorf("%s", tok) }
 
 	tok, err = l.NextToken()
 	if err != nil { t.Errorf(err.Error()) }
-	if tok.Type != TOK_INT && tok.Val != 1234 { t.Errorf("%s", tok) }
+	if tok.Type != TOK_INT || tok.Val != 1234 { t.Errorf("%s", tok) }
 	
 	tok, err = l.NextToken()
 	if err != nil { t.Errorf(err.Error()) }
-	if tok.Type != TOK_FLOAT && tok.Val != 1234.12 { t.Errorf("%s", tok) }
+	if tok.Type != TOK_FLOAT || tok.Val != 1234.12 { t.Errorf("%s", tok) }
+
+	tok, err = l.NextToken()
+	if err != nil { t.Errorf(err.Error()) }
+	if tok.Type != TOK_STR || tok.Val != "test string" { t.Errorf("%s", tok) }
+
 }
